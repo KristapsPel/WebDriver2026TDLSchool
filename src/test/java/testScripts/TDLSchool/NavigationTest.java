fc52ff48e95c4c20717477701a667d7532a23345
KristapsPel <kristaps.pelcbergs@testdevlab.com>
2026-09-14 13:08:39 +0300

    PT7 Report

--- src/test/java/testScripts/TDLSchool/NavigationTest.java
+++ src/test/java/testScripts/TDLSchool/NavigationTest.java
@@ -1,13 +1,27 @@
 package testScripts.TDLSchool;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.Test;
 import pages.CareerPage;
 import pages.FooterPage;
 import pages.HeaderPage;
 import pages.HomePage;
 
+import java.io.File;
+
 
 public class NavigationTest extends BasePage {
+//    Open project where we did test on TDLschool page use method from Practice 4 Done
+//    Add Extent Report Dependency Done
+//    Create report and test objects Done
+//    Replace all System.out.println wit test logs with Status +/-Done
+//    Add screenshot on last step Done
 
     @Test(testName = "TDL School navigation",
             description = "We check navigation on TDL School homepage")
@@ -23,10 +37,29 @@ public class NavigationTest extends BasePage {
         CareerPage careerPage = new CareerPage(driver);
         FooterPage footerPage = new FooterPage(driver);
 
-        headerPage.clickCareerPaths();
-        careerPage.validateTitleValue("Not sure where to start?");
-        footerPage.enterEmail("test");
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(
+                System.getProperty("user.dir") + File.separator +
+                "report" + File.separator +
+                "TestReport.html");
 
+        ExtentReports extentReports = new ExtentReports();
+        extentReports.attachReporter(sparkReporter);
+
+        ExtentTest extentTest = extentReports.createTest("TDL School navigation test",
+                "We are checking if navigation inside TDL School page work correctly");
+
+        extentTest.log(Status.INFO, "Click on Career Paths button");
+        headerPage.clickCareerPaths();
+
+        extentTest.log(Status.INFO, "We check if Not sure where to start? title is visible");
+        careerPage.validateTitleValue("Not sure where to start?");
+        extentTest.log(Status.PASS, "Title is correct");
+
+        extentTest.log(Status.INFO, "We enter test inside email input field in footer");
+        footerPage.enterEmail("test");
+        addScreenshotToReport(Status.INFO, extentTest);
+
+        extentReports.flush();
     }
 
     @Test
@@ -34,6 +67,15 @@ public class NavigationTest extends BasePage {
         HomePage homePage = new HomePage(driver);
 
         homePage.validateUpcomingCourseCount(4);
+    }
+
+    public void addScreenshotToReport(Status status, ExtentTest test) {
+        // Capture the screenshot as a Base64 string and prepend with the data URI scheme for a PNG image
+        String base64ScreenShot = "data:image/png;base64," + ((TakesScreenshot) driver)
+                .getScreenshotAs(OutputType.BASE64);
+
+        // Log the screenshot to the report with the specified status (PASS, FAIL, etc.)
+        test.log(status, MediaEntityBuilder.createScreenCaptureFromBase64String(base64ScreenShot).build());
     }
 
 }
