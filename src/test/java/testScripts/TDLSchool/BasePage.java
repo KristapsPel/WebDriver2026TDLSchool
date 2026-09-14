434bdb81d4a77c7330e7c1734085b11815c9aaee
KristapsPel <kristaps.pelcbergs@testdevlab.com>
2026-09-14 13:46:35 +0300

    PT9 Report

--- src/test/java/testScripts/TDLSchool/BasePage.java
+++ src/test/java/testScripts/TDLSchool/BasePage.java
@@ -1,16 +1,41 @@
 package testScripts.TDLSchool;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import pages.HeaderPage;
 import utils.WebDriverHelper;
 
+import java.io.File;
+import java.lang.reflect.Method;
+
 public class BasePage {
     protected WebDriver driver;
+    private ExtentReports report;
+    public ExtentTest extentTest;
+
+    @BeforeSuite
+    public void createReport(){
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(
+                System.getProperty("user.dir") + File.separator +
+                        "report" + File.separator +
+                        "TestReport.html");
+
+        report = new ExtentReports();
+        report.attachReporter(sparkReporter);
+    }
+
+    @AfterSuite
+    public void generateReport() {
+        report.flush();
+    }
 
     @BeforeMethod
-    public void setUpBrowser(){
+    public void setUpBrowser(Method method){
+        extentTest = report.createTest(method.getAnnotation(Test.class).testName(),
+                method.getAnnotation(Test.class).description());
         driver = WebDriverHelper.setUpDriverWithWDM("CHROME");
         driver.manage().window().maximize();
         driver.get("https://tdlschool.com/");
--- src/test/java/testScripts/TDLSchool/NavigationTest.java
+++ src/test/java/testScripts/TDLSchool/NavigationTest.java
@@ -17,11 +17,10 @@ import java.io.File;
 
 
 public class NavigationTest extends BasePage {
-//    Open project where we did test on TDLschool page use method from Practice 4 Done
-//    Add Extent Report Dependency Done
-//    Create report and test objects Done
-//    Replace all System.out.println wit test logs with Status +/-Done
-//    Add screenshot on last step Done
+//    Create Report before all test begin
+//    Create test before each Test method
+//    Export Report after all tests are done
+
 
     @Test(testName = "TDL School navigation",
             description = "We check navigation on TDL School homepage")
@@ -37,17 +36,6 @@ public class NavigationTest extends BasePage {
         CareerPage careerPage = new CareerPage(driver);
         FooterPage footerPage = new FooterPage(driver);
 
-        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(
-                System.getProperty("user.dir") + File.separator +
-                "report" + File.separator +
-                "TestReport.html");
-
-        ExtentReports extentReports = new ExtentReports();
-        extentReports.attachReporter(sparkReporter);
-
-        ExtentTest extentTest = extentReports.createTest("TDL School navigation test",
-                "We are checking if navigation inside TDL School page work correctly");
-
         extentTest.log(Status.INFO, "Click on Career Paths button");
         headerPage.clickCareerPaths();
 
@@ -58,27 +46,15 @@ public class NavigationTest extends BasePage {
         extentTest.log(Status.INFO, "We enter test inside email input field in footer");
         footerPage.enterEmail("test");
         addScreenshotToReport(Status.INFO, extentTest);
-
-        extentReports.flush();
     }
 
-    @Test
+    @Test(testName = "Validate visible courses count on Homepage")
     public void checkCoursesCount() {
         HomePage homePage = new HomePage(driver);
-        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(
-                System.getProperty("user.dir") + File.separator +
-                        "report" + File.separator +
-                        "TestReport2.html");
 
-        ExtentReports extentReports = new ExtentReports();
-        extentReports.attachReporter(sparkReporter);
-
-        ExtentTest extentTest = extentReports.createTest("Check upcoming lectures");
         extentTest.log(Status.INFO, "Check that 4 elements are visible inside upcoming lectures");
         homePage.validateUpcomingCourseCount(4);
         extentTest.log(Status.PASS, "Upcoming lecture shows correct amount of courses");
-
-        extentReports.flush();
     }
 
     public void addScreenshotToReport(Status status, ExtentTest test) {
